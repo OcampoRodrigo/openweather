3f28a145b7a3c721bd2df822dc0fb23e5bbc41e7
Rodrigo Ocampo <ocampoirodrigo@gmail.com>
2023-08-18 22:40:37 -0300

    Arreglo con columnas extra en el csv y proceso de carga en la base de datos

--- main.py
+++ main.py
@@ -13,28 +13,52 @@ BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
-os.makedirs("data_analytics/openweather", exist_ok = True)
+#Variable para trackear los headers(nombre de las columnas) en el archivo csv y cargarlos solo una vez
+headers_written = False
+key_columns = []
 
+
+#Creamos el directorio para guardar los archivos csv
+directory_path = "data_analytics/openweather"
+os.makedirs(directory_path, exist_ok = True)
 
 # Función para obtener los datos de clima de una ciudad y convertirlos a CSV
 def get_weather_data(city, coordinates):
     params = {
         "q": city,
-        "appid": api_key  
+        "appid": "7d562f62c8e7c442135ef6e459443cee"  # Agregar tu API Key de OpenWeatherMap aquí
     }
     response = requests.get(BASE_URL, params=params)
     if response.status_code == 200:
+        global headers_written, key_columns
         data = response.json()
         df = json_normalize(data)
-        df_weather = json_normalize(data, record_path="weather")
-        df = df.drop(columns=["weather"])
-        df = pd.concat([df,df_weather], axis=1)
+        df_weather = json_normalize(data, record_path="weather")        
+        df = df.drop(columns=["weather"])         
         date_str = datetime.utcfromtimestamp(data["dt"]).strftime('%Y%m%d')
         file_path = f"data_analytics/openweather/tiempodiario_{date_str}.csv"
-        df.to_csv(file_path, index=False, mode="a")
-        print(f"Datos de clima de {city} almacenados en {file_path}.")
+        if not headers_written:          
+          df = pd.concat([df_weather,df], axis=1)
+          df["city"] = city          
+          df.to_csv(file_path, index=False, mode="a")
+          headers_written = True
+          
+          #Guardamos las columnas de la 1er respuesta,
+          # ya que hay variaciones en la respuesta de la API
+          key_columns = df.columns.tolist()
+          print(f"Datos de clima de {city} almacenados en {file_path}. Con headers")
+        else:          
+          # Si no esta dentro de las columnas de la 1er respuesta, dropeamos columnas extra
+          # para tener consistencia en el archivo csv          
+          columns_to_drop = [col for col in df.columns if col not in key_columns]
+          df = df.drop(columns=columns_to_drop)          
+          df = pd.concat([df_weather,df], axis=1)
+          df["city"] = city
+          print(df.shape)
+          df.to_csv(file_path, index=False, mode="a", header=False)
+          print(f"Datos de clima de {city} almacenados en {file_path}.")
     else:
-        print(f"Error al obtener los datos de clima de {city}.")
+        print(f"Error al obtener los datos de clima de {city}.", response.status_code)
 
 
 # Lista de ciudades y coordenadas correspondientes
@@ -46,13 +70,11 @@ coordList = ["lat=31&lon=64", "lat=40&lon=-73", "lat=-31&lon=-64", "lat=25&lon=6
 for city, coord in zip(cityList,coordList):
     get_weather_data(city, coord)
 
-#
-
 # Leer las credenciales de conexión a la base de datos desde el archivo config.py
-    config = Config()
+config = Config()
 
 # Establecer la conexión a la base de datos PostgreSQL utilizando SQLalchemy
-#engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
 
 # Función para cargar datos de CSV a la base de datos
 def load_data_to_database(csv_path):
@@ -63,7 +85,13 @@ def load_data_to_database(csv_path):
 
 # Ruta de los archivos CSV generados previamente
 csv_paths = []
-#csv_paths.append(file_path)
+for filename in os.listdir(directory_path):
+    # Check if the item is a file (not a directory)
+    if os.path.isfile(os.path.join(directory_path, filename)):
+        # Add the full path of the file to the list
+        csv_paths.append(os.path.join(directory_path, filename))
+
 # Cargar datos de todos los archivos CSV a la base de datos
-#for csv_path in csv_paths:
-#    load_data_to_database(csv_path)
+for csv_path in csv_paths:
+    load_data_to_database(csv_path)
+    print("load succesful")
